27eeefe3e2471b930a05f98779c2f4566f1662f4
unknown <greateducation123@yahoo.com>
2023-03-07 14:26:19 -0600

    Created Views Function and urls File for notes app

--- backend/drf_jwt_backend/urls.py
+++ backend/drf_jwt_backend/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/cars/', include('cars.urls')),
-    path('api/care_requests/', include('care_requests.urls'))
+    path('api/care_requests/', include('care_requests.urls')),
+    path('api/notes/', include('notes.urls'))
 ]
