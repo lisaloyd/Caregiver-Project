436d79b455649dd9c6c93fe9123f2acb78775f5c
unknown <greateducation123@yahoo.com>
2023-03-07 14:50:44 -0600

    Created Capstone Collection in Postman and added Care_Request Folder + updated views Function in care_request app

--- backend/care_requests/views.py
+++ backend/care_requests/views.py
@@ -1,8 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import Care_RequestSerializer
+from .models import Care_Request
 
 @api_view(['GET'])
 def care_requests_list(request):
+    care_requests = Care_Request.objects.all()
 
+    serializer = Care_RequestSerializer(care_requests, many=True)
 
-    return Response('ok')
+    return Response(serializer.data)
