b0811627fee3dbca91209fce485ac79c91527462
unknown <greateducation123@yahoo.com>
2023-03-07 15:05:51 -0600

    Created Notes Folder to Capstone Collection Folder in Postman + updated views Function in notes app

--- backend/notes/serializers.py
+++ backend/notes/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Note
 
-class NotesSerializer(serializers.ModelSerializer):
+class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
         fields = ['id', 'notes', 'care_request_id']
--- backend/notes/views.py
+++ backend/notes/views.py
@@ -1,9 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import NoteSerializer
+from .models import Note
+
+
 
 @api_view(['GET'])
 def notes_list(request):
+    notes = Note.objects.all()
+
+    serializer = NoteSerializer(notes, many=True)
 
 
-    return Response('ok')
+    return Response(serializer.data)
 
