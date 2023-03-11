063c606e1421fc29a5bfb05cdc07cfa2e2137677
unknown <greateducation123@yahoo.com>
2023-03-11 14:07:20 -0600

    Modified components

--- frontend/src/components/Authentication/Authentication.jsx
+++ frontend/src/components/Authentication/Authentication.jsx
@@ -1,10 +1,64 @@
 import React, { useState } from 'react';
+import Login from './Login';
+import Register from './Register';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col, } from 'reactstrap';
+import classnames from 'classnames';
+import './Authentication.css';
 
 const Authentication = (props) => {
+    const [activeTab, setActiveTab] = useState('1');
+
+    const toggle = tab => {
+        if(activeTab !== tab) setActiveTab(tab);
+    }
     return (
         <div>
-            Authentication
+            <Row><h1>&nbsp;</h1></Row>
+            {/* <div style={{ textAlign: 'center', marginBottom: '5%'}} ><h1 >SALTY THINGS</h1></div> */}
+            <Nav tabs >
+                <NavItem style={{width: "50%"}}>
+                    <NavLink 
+                        className={classnames({ active: activeTab === '1' })}
+                        onClick={() => { toggle('1'); }}
+                    >
+                        Login
+                    </NavLink>
+                </NavItem>
+                <NavItem style={{width: "50%"}}>
+                    <NavLink
+                        className={classnames({ active: activeTab === '2' })}
+                        onClick={() => { toggle('2'); }}
+                    >
+                        Register
+                    </NavLink>
+                </NavItem>
+                
+                
+            </Nav>
+            <TabContent activeTab={activeTab}>
+                <TabPane tabId="1">
+                <Row>
+                    <Col sm="12">
+                        <div classname='jumbotron' style={{backgroundImage: 'linear-gradient(#04519b, #033C73 60%, #02325f)'}}>
+                            <Login />
+                        </div>
+                    </Col>
+                </Row>
+                </TabPane>
+                <TabPane tabId="2">
+                <Row>
+                    <Col sm="12">
+                        <div classname='jumbotron' style={{backgroundImage: 'linear-gradient(#6d94bf, #446E9B 50%, #3e648d)'}}>
+                            <Register />
+                        </div>
+                    </Col>
+                </Row>
+                </TabPane>
+                
+                
+            </TabContent>
         </div>
-    )
+    );
 }
+    
 export default Authentication
--- frontend/src/components/Authentication/Login.jsx
+++ frontend/src/components/Authentication/Login.jsx
@@ -1,10 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from "react";
+import AuthContext from "../../context/AuthContext";
+import useCustomForm from "../../hooks/useCustomForm";
+import { Link } from "react-router-dom";
+import "./Login.css";
+
 
 const Login = (props) => {
-    return (
-        <div>
-            Login
-        </div>
-    )
-}
-export default Login
+  const { loginUser, isServerError } = useContext(AuthContext);
+  const defaultValues = { username: "", password: "" };
+  const [formData, handleInputChange, handleSubmit, reset] = useCustomForm(
+    defaultValues,
+    loginUser
+  );
+  useEffect(() => {
+    if (isServerError) {
+      reset();
+    }
+  }, [isServerError]);
+
+  return (
+    <div className="container">
+      <form className="form" onSubmit={handleSubmit}>
+        <label>
+          Username:{" "}
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Password:{" "}
+          <input
+            type="text"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+        </label>
+        {isServerError ? (
+          <p className="error">Login failed, incorrect credentials!</p>
+        ) : null}
+        <Link to="/register">Click to register!</Link>
+        <button>Login!</button>
+      </form>
+    </div>
+  );
+};
+
+
+export default Login;
