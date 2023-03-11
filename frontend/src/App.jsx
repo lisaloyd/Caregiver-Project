b555a06360599f7a25e9a020e815444a0ecdea04
unknown <greateducation123@yahoo.com>
2023-03-11 11:54:26 -0600

    App is loading without errors and component hierarchy tree is loading correctly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 // Pages Imports
-import HomePage from "./pages/HomePage/HomePage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import Home from "./components/Home/Home";
+import Authentication from "./components/Authentication/Authentication";
+import ContractorDashboard from "./components/ContractorDashboard/ContractorDashboard";
+import ClientDashboard from "./components/ClientDashboard/ClientDashboard";
+import Search from "./components/Search/Search";
 
 // Component Imports
 import NavBarClient from "./components/NavBarClient/NavBarClient";
@@ -15,21 +17,32 @@ import Footer from "./components/Footer/Footer";
 // Util Imports
 import PrivateRoute from "./utils/PrivateRoute";
 
+
 function App() {
   return (
     <div>
-      <Navbar />
+      <NavBarContractor />
+      <NavBarClient />
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Authentication />} />
+        <Route path="/search" element={<Search />} />
         <Route
-          path="/"
+          path="/client/"
           element={
             <PrivateRoute>
-              <HomePage />
+              <ClientDashboard />
             </PrivateRoute>
           }
         />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/contractor/"
+          element={
+            <PrivateRoute>
+              <ContractorDashboard />
+            </PrivateRoute>
+          }
+        />
       </Routes>
       <Footer />
     </div>
