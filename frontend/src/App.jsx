0c5e7122acb74d5208b6eb0e60977a718dcad121
unknown <greateducation123@yahoo.com>
2023-03-16 10:42:47 -0500

    Updated route path in app.jsx so the main page is the LoginPage + updated ClientDashboard to complete first user story

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,15 +6,15 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import "./App.css";
 
 // Pages Imports
-import Home from "./components/Home/Home";
-import Authentication from "./components/Authentication/Authentication";
-import ContractorDashboard from "./components/ContractorDashboard/ContractorDashboard";
-import ClientDashboard from "./components/ClientDashboard/ClientDashboard";
+// import Home from "./components/Home/Home";
+// import Authentication from "./components/Authentication/Authentication";
+// import ContractorDashboard from "./components/ContractorDashboard/ContractorDashboard";
+// import ClientDashboard from "./components/ClientDashboard/ClientDashboard";
 import LoginPage from "./pages/LoginPage/LoginPage";
 
 // Component Imports
-import NavBarClient from "./components/NavBarClient/NavBarClient";
-import NavBarContractor from "./components/NavBarContractor/NavBarContractor";
+//  NavBarClient from "./components/NavBarClient/NavBarClient";
+// import NavBarContractor from "./components/NavBarContractor/NavBarContractor";
 import Footer from "./components/Footer/Footer";
 
 // Util Imports
@@ -24,80 +24,15 @@ import Navbar from "./components/Navbar/Navbar";
 import DashboardPage from "./pages/DashboardPage/DashboardPage";
 
 function App() {
-  //   const BASE_URL = "http://127.0.0.1:8000/api/care_requests/";
-  //   const [search, setSearch] = useState(null);
-  //   const [notes, setNotes] = useState([]);
-
-  //   const [isServerError, setIsServerError] = useState(false);
-  //   const navigate = useNavigate();
-  //   const id = cuid();
-  //   const submitNote = async(noteData) => {
-  //     try {
-  //       let finalData = {
-  //         note: noteData.note
-  //       }
-  //       let response = await axios.post(`${BASE_URL}/${id}/notes/`, finalData)
-  //       if (response.status === 201) {
-  //         setNotes([...notes, response.data])
-  //         setIsServerError(false);
-
-  //         navigate("/client");
-  //       } else {
-  //         navigate("/register");
-  //       }
-  //     } catch (error) {
-  //       console.log (error)
-  //     }
-  //   }
-  //   const submitSearch = async(searchData) => {
-  //     try {
-
-  //       let response = await axios.get(`${BASE_URL}/zipcode/${searchData}`)
-  //       if (response.status === 200) {
-  //         setIsServerError(false);
-  //         setSearch(response.data);
-  //         navigate("/search");
-  //       } else {
-  //         navigate("/error");
-  //       }
-  //     } catch (error) {
-  //       console.log(error)
-  //     }
-  //   }
   return (
     <div>
        <Navbar/> 
-       {/* <NavBarContractor />
-      <NavBarClient />  */}
       <Routes>
-      <Route path='/' element={<DashboardPage/>}/>
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/auth" element={<Authentication />} />
+        <Route path='/' element={<LoginPage/>}/>
+        <Route path="/dashboard" element={<DashboardPage />} />
+        {/* <Route path="/auth" element={<Authentication />} /> */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        
-        <Route path="/clientdashboard" element={<ClientDashboard />} />
-        {/* <Route path="/" element={<Home submitSearch={submitSearch}/>} /> */}
-        {/* <Route path="/auth" element={<Authentication />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />}/>
-        <Route path='/dashboard' element={<DashboardPage/>}/> */}
-        {/* <Route
-          path="/client/"
-          element={
-            <PrivateRoute>
-              <ClientDashboard submitNote={submitNote}/>
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/contractor/"
-          element={
-            <PrivateRoute>
-              <ContractorDashboard />
-            </PrivateRoute>
-          }
-        /> */}
       </Routes>
       <Footer />
     </div>
--- frontend/src/components/ClientDashboard/ClientDashboard.jsx
+++ frontend/src/components/ClientDashboard/ClientDashboard.jsx
@@ -2,11 +2,17 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 // import useCustomForm from "../../hooks/useCustomForm";
 
+
 const ClientDashboard = () => {
   const [searchContractors, setSearchContractors] = useState([])
   const [allResponse, setAllResponse] = useState([])
 const[user,setUser]=useState()
 const[userdetails,setUserdetails]=useState([])
+const [flag,setFlag]=useState(false)
+const [show,setShow]=useState(false)
+const [quary,setQueary]=useState('')
+const [date,setDate]=useState(new Date)
+console.log("quary and date",quary,date)
 
 useEffect(()=>{
   getSearchContractors()
@@ -38,13 +44,30 @@ let userDetails= allResponse.filter(data=>{
   console.log("user",user)
   return (  
    <div>
-    <input type='text'  onChange={(e)=>setUser(e.target.value)} />
+    <input type='text'  onChange={(e)=>setUser(e.target.value)}className='form-control' placeholder='Please type zipcode here...' style={{width:"20%"}} />
     <button onClick={submit}>Submit</button>
    { userdetails.map(data=>{
       return(
         <p>{data?.username}</p>
       )
     })}
+    <button onClick={()=>setFlag(true)} className="btn-success">Help</button>
+    {
+      flag ?
+      <div>
+        <input type="text" className='form-control' onChange={(e)=>setQueary(e.target.value)}  placeholder='Please type your need here...' style={{width:"60%"}} />
+        <input type='date'className='form-control' onChange={(e)=>setDate(e.target.value)} style={{width:"60%"}}/>
+        <button onClick={()=>setShow(true)}>Submit</button>
+        {show?
+          <div>
+           <p>{quary}</p>
+         <p>{date}</p>
+         </div>
+         :null
+        }
+      </div>
+      :null
+    }
    </div> 
   );
 }
